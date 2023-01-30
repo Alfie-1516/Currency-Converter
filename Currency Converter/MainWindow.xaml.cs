ce83642bce6dd4499ad8788117a56ac7dfe30900
Eleoper Mapute <emapute@mcneese.edu>
2023-01-30 14:12:03 -0600

    completed

--- Currency Converter/MainWindow.xaml.cs
+++ Currency Converter/MainWindow.xaml.cs
@@ -21,26 +21,14 @@ namespace Currency_Converter
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int currency;
+        public string currency1;
+        public string currency2;
         public MainWindow()
         {
             InitializeComponent();
-        }
-        private void load_combobox_right(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem item = (ComboBoxItem)right_combo_box.SelectedItem;
-            currency = right_combo_box.SelectedIndex;
-            Trace.WriteLine(currency);
-        }
-        private void convert(object sender, RoutedEventArgs e)
-        {
-            Text_right.Text = Text_left.Text;
-            Trace.WriteLine(right_combo_box.Items.Count);
-            Trace.WriteLine(right_combo_box.Items.IndexOf(0));
-          
-           
-        }
+            
 
+        }
 
         private void minimize_win(object sender, RoutedEventArgs e)
         {
@@ -54,5 +42,24 @@ namespace Currency_Converter
         {
             Close();
         }
+
+        private void left_combo_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selected_value = (ComboBoxItem)left_combo_box.SelectedItem;
+            currency1 = selected_value.Name;
+            Text_left.Text = currency1;  
+        }
+
+        private void right_combo_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selected_value = (ComboBoxItem)right_combo_box.SelectedItem;
+            currency2 = selected_value.Name;
+            Text_right.Text = currency2;
+
+
+
+        }
+
+
     }
 }
